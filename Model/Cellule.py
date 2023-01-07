ebfcb1acb0b38cde8993aba169a4b565f1111b80
Romain Poirrier <romain.poirrier@etudiant.univ-reims.fr>
2023-01-07 17:36:52 +0100

    isContenuCorrect : tests

--- Model/Cellule.py
+++ Model/Cellule.py
@@ -19,4 +19,15 @@ def type_cellule(cell: dict) -> bool:
         and type(cell[const.VISIBLE] == bool) and type(cell[const.CONTENU]) == int \
         and (0 <= cell[const.CONTENU] <= 8 or cell[const.CONTENU] == const.ID_MINE)
 
+def isContenuCorrect(content: int) -> bool:
+    """
+    Cette fonction vérifie si un entier peut représenter le contenu d'une cellule
+    :param content: entier
+    :return: True si l'entier est entre 0 et 8 ou s'il vaut la valeur de const.ID_MINE
+    """
+    if type(content) != int :
+        ret = False
+    else :
+        ret = ( content <= 8 and content >= 0 ) or content == const.ID_MINE
 
+    return ret
